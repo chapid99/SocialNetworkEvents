ade6ff2898d4e3572e2fa3fc3ad23f1285eaea4b
MiguelChapid <chapid99@gmail.com>
2020-05-09 21:29:52 -0500

    Modificaciones de base de datos

---
+++ database/migrations/2020_05_10_022112_add_foreign_key_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeyUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->bigInteger('typeuser_id')->unsigned();
+            $table->foreign('typeuser_id')
+                 ->references('id')->on('type_users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_type_users_id_foreign');
+        });
+    }
+}
